44437ef15fdf4226352fee01a34e68837ef61043
Dubaai <63336110+Dubaai@users.noreply.github.com>
2022-12-27 15:28:36 -0300

    Chefe

---
+++ script.js
@@ -0,0 +1,116 @@
+const BOSSES = [];
+let interval;
+
+const requestPermission = () => {
+
+    if (Notification.permission !== "granted") {
+
+        alert("Ative as notificações para utilizar a ferramenta.");
+
+        Notification.requestPermission().then(permission => {
+
+            if(permission !== "granted"){
+                requestPermission();
+            }else{
+                console.log("Permissão garantida.");
+            }
+
+        });
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", evt => {
+    requestPermission();
+
+    let times = document.querySelectorAll('input[type="time"]');
+
+    times.forEach(el =>{
+
+        let horaAgora = new Date();
+        el.value = `${horaAgora.getHours().toString().padStart(2,'0')}:${horaAgora.getMinutes().toString().padStart(2,'0')}`
+
+    })
+
+    interval = setInterval(() => {
+
+        times.forEach(el => {
+
+            let horaAgora = new Date();
+            let boss = BOSSES[el.getAttribute("tag")];
+
+            if(!boss){
+
+                el.value = `${horaAgora.getHours().toString().padStart(2,'0')}:${horaAgora.getMinutes().toString().padStart(2,'0')}`
+
+            }else{
+
+                let horaFinal = new Date(boss.resetTime)
+
+                el.value = `${horaFinal.getHours().toString().padStart(2,'0')}:${horaFinal.getMinutes().toString().padStart(2,'0')}`
+
+                updateStatus(el.getAttribute("tag"))
+
+                if(boss.resetTime <= horaAgora.getTime()){
+
+                    delete BOSSES[el.getAttribute("tag")];
+                    doAlert(el.getAttribute("tag"));
+
+                }
+
+            }
+        })
+    }, 500)
+
+})
+
+const onBossDeath = (boss, type) => {
+
+    let a = 0;
+    if (type === 'YELLOW') a = 59.5;
+    if (type === 'GREEN') a = 29.5;
+    if (type === '1Goblin') a = 15;
+    if (type === '2Goblin') a = 15;
+    if (type === 'MINAYELLOW') a = 59.5;
+    if (type === 'BAU_YELLOW') a = 59.5;
+    if (type === 'BAURED') a = 29.5;
+    if (type === 'LIDERI') a = 29.5;
+    if (type === 'LIDERII') a = 59.5;
+    if (type === 'TESTE') a = 0.2;
+    BOSSES[boss] = {
+        
+        resetTime: (new Date().getTime() + (a * 60 * 1000)),
+
+
+        alive: false
+    }
+
+    updateStatus(boss)
+
+    console.log(BOSSES)
+}
+
+const doAlert = (boss) => {
+
+    let name = document.querySelectorAll('input[type="text"]' + `[tag="${boss}"]`)[0];
+    let bossName = name.value ? name.value : name.placeholder;
+
+    new Notification("Seu boss vai nascer em breve.", {
+        body: "Boss: " + bossName + ".",
+        icon: "./src/logo.png"
+    })
+
+    updateStatus(boss)
+}
+
+const updateStatus = (boss) => {
+
+    let status = document.getElementById(`${boss}`);
+
+    if(BOSSES[boss]){
+        status.style.backgroundColor = "#ff0000"
+    }else{
+        status.style.backgroundColor = "#08d202"
+    }
+
+}
